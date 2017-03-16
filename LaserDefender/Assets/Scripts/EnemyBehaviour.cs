16e1af474c51dcfdb283f11dc7832c0c913a7920
Canh Doan <canhdoan@z3r0hihi.local>
2017-03-16 21:40:47 +0700

    - Attribute and function control a enemy ship is implement in EnemyBehaviour script

---
+++ LaserDefender/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,36 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2017 AsNet Co., Ltd.
+// All Rights Reserved. These instructions, statements, computer
+// programs, and/or related material (collectively, the "Source")
+// contain unpublished information proprietary to AsNet Co., Ltd
+// which is protected by US federal copyright law and by
+// international treaties. This Source may NOT be disclosed to
+// third parties, or be copied or duplicated, in whole or in
+// part, without the written consent of AsNet Co., Ltd.
+////////////////////////////////////////////////////////////////////////////////
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+	public float health = 150.0f;
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Projectile laser = other.gameObject.GetComponent<Projectile>();
+		if (laser)
+		{
+			health -= laser.Damage;
+			laser.Hit();
+
+			if (health <= 0.0f)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
+
