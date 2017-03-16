7ddb0d8b46dcecd6572518b07ecd969d1b5fc2a3
Canh Doan <canhdoan@z3r0hihi.local>
2017-03-16 21:41:56 +0700

    - Helper script used to handle destoy laser bullet when it's going out of play space

---
+++ LaserDefender/Assets/Scripts/Shredder.cs
@@ -0,0 +1,31 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2017 AsNet Co., Ltd.
+// All Rights Reserved. These instructions, statements, computer
+// programs, and/or related material (collectively, the "Source")
+// contain unpublished information proprietary to AsNet Co., Ltd
+// which is protected by US federal copyright law and by
+// international treaties. This Source may NOT be disclosed to
+// third parties, or be copied or duplicated, in whole or in
+// part, without the written consent of AsNet Co., Ltd.
+////////////////////////////////////////////////////////////////////////////////
+using UnityEngine;
+
+namespace Namespace.Shredder
+{
+	/// <summary>
+	/// Shredder
+	/// </summary>
+	public class Shredder : MonoBehaviour
+	{
+		/// <summary>
+		/// Sent when an incoming collider makes contact with this object's
+		/// collider (2D physics only).
+		/// </summary>
+		/// <param name="other">The Collision2D data associated with this collision.</param>
+		void OnCollisionEnter2D(Collision2D other)
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
+
