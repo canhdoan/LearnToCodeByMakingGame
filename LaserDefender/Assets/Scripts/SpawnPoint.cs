8ab6dbea96db401b9a57a05a534d4d5298e25fb1
Canh Doan <canhdoan@z3r0hihi.local>
2017-03-16 21:41:57 +0700

    - Script used to format enemies ship on scene

---
+++ LaserDefender/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,31 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2017 AsNet Co., Ltd.
+// All Rights Reserved. These instructions, statements, computer
+// programs, and/or related material (collectively, the "Source")
+// contain unpublished information proprietary to AsNet Co., Ltd
+// which is protected by US federal copyright law and by
+// international treaties. This Source may NOT be disclosed to
+// third parties, or be copied or duplicated, in whole or in
+// part, without the written consent of AsNet Co., Ltd.
+////////////////////////////////////////////////////////////////////////////////
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Namespace.SpawnPoint
+{
+	/// <summary>
+	/// SpawnPoint
+	/// </summary>
+	public class SpawnPoint : MonoBehaviour
+	{
+		/// <summary>
+		/// Callback to draw gizmos that are pickable and always drawn.
+		/// </summary>
+		void OnDrawGizmos()
+		{
+			Gizmos.DrawWireSphere(transform.position, 1.0f);
+		}
+	}
+}
+
